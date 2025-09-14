aa90527520349a999e150a4793ba2dcd0a022f13
Fauqi Amar Hizbullah <a@fauqi.com>
2025-09-14 18:19:19 +0700

    feat: Update User controller

--- src/controllers/user.rs
+++ src/controllers/user.rs
@@ -2,20 +2,18 @@
 #![allow(clippy::unnecessary_struct_initialization)]
 #![allow(clippy::unused_async)]
 use loco_rs::prelude::*;
-use serde::{ Deserialize, Serialize };
 use sea_orm::{ sea_query::Order, QueryOrder };
 use axum::debug_handler;
 
 use crate::{ models::_entities::users::{ Column, Entity } };
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Params {
-  pub url: String,
-  pub short_url: String,
+#[debug_handler]
+pub async fn hello(State(_ctx): State<AppContext>) -> Result<Response> {
+  format::json(serde_json::json!({ "message": "Hello World" }))
 }
 
 #[debug_handler]
-pub async fn index(State(ctx): State<AppContext>) -> Result<Response> {
+pub async fn users(State(ctx): State<AppContext>) -> Result<Response> {
   let users = Entity::find()
     .order_by(Column::Id, Order::Desc)
     .all(&ctx.db).await?;
@@ -23,5 +21,8 @@ pub async fn index(State(ctx): State<AppContext>) -> Result<Response> {
 }
 
 pub fn routes() -> Routes {
-  Routes::new().prefix("/api/users").add("/", get(index))
+  Routes::new()
+    .prefix("/api")
+    .add("/hello", get(hello))
+    .add("/users", get(users))
 }
